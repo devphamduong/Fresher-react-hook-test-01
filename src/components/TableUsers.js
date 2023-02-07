a45bd4bc704f6d0e35d15b932f38ce4f0e926ad2
Pham Chu Duong <pduong244@gmail.com>
2023-02-07 10:28:12 +0700

    Table Users with paginate

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,19 +1,28 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { fetchAllUser } from '../services/UserService';
+import ReactPaginate from 'react-paginate';
 
 function TableUsers(props) {
     const [listUsers, setListUsers] = useState([]);
+    const [totalUsers, setTotalUsers] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect(() => {
-        getDataUsers();
+        getDataUsers(1);
     }, []);
 
-    const getDataUsers = async () => {
-        let res = await fetchAllUser();
+    const getDataUsers = async (page) => {
+        let res = await fetchAllUser(page);
         if (res && res.data && res.data) {
             setListUsers(res.data);
+            setTotalUsers(res.total);
+            setTotalPages(res.total_pages);
         }
+    };
+
+    const handlePageClick = (event) => {
+        getDataUsers(+event.selected + 1);
     };
 
     return (
@@ -47,6 +56,25 @@ function TableUsers(props) {
                     }
                 </tbody>
             </Table>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={totalPages}
+                previousLabel="< previous"
+                pageClassName='page-item'
+                pageLinkClassName='page-link'
+                previousClassName='page-item'
+                previousLinkClassName='page-link'
+                nextClassName='page-item'
+                nextLinkClassName='page-link'
+                breakClassName='page-item'
+                breakLinkClassName='page-link'
+                containerClassName='pagination'
+                activeClassName='active'
+                renderOnZeroPageCount={null}
+            />
         </>
     );
 }
